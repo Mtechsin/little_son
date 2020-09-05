6f65318307634bbf501d66ec2f682c5bcae0a7a7
Mtechsin <ahmedson3001@gmail.com>
2020-09-05 21:50:28 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import discord
 from discord.ext import commands
 import os
-bot = commands.Bot(command_prefix = '*')
+bot = commands.Bot(command_prefix = '+')
+@bot.remove_command("help")
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game('playing *help'))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity('Annoying you 😋'))
     print("ready")
+@bot.command()
+async def help(ctx):
+    emb = discord.Embed(
+        title=f"Commands for {bot.user.name}",
+        colour=discord.colour.Colour.from_rgb(81, 147, 252)
+    )
+    await ctx.send(embed=emb)
+@bot.command()
+async def ping(ctx):
+    emb = discord.Embed(
+        colour=discord.colour.Colour.from_rgb(81, 147, 252)
+    )
+    emb.add_field(name="Your Ping is ", value=f'```{round(bot.latency*1000)} ms              ```')
+    await ctx.send("🏓 Pong!  ")
+    await ctx.send(embed = emb)
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-
-bot.run("NzUwNDY0NzU2MTY2NTU3NzY4.X066xA.iQqGbPqAiYzwr0287ZKBabnoZSI")
+home = os.environ['token']
+bot.run(home)
